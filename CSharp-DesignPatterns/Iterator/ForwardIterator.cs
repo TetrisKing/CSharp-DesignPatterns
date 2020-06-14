33c0accad55e42ceb2b7d37667e6f9a015d9c0c6
Unknown <TetrisCurry@gmail.com>
2020-06-14 07:50:01 +0100

    Iterator Pattern
    Iterator 2

---
+++ CSharp-DesignPatterns/Iterator/ForwardIterator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_DesignPatterns.Iterator
+{
+    public class ForwardIterator : IEnumerator
+    {
+        private readonly List<int> collection;
+        int currentPosition;
+
+        public ForwardIterator(List<int> collection)
+        {
+            this.collection = collection;
+            currentPosition = 0;
+        }
+
+        public object Current => collection[currentPosition];
+
+        public bool MoveNext()
+        {
+            if (!HasMore())
+                return false;
+            currentPosition++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentPosition = 0;
+        }
+        public bool HasMore()
+        {
+            return collection.Count - 1 > currentPosition;
+        }
+    }
+}
---
+++ CSharp-DesignPatterns/Iterator/IIterator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_DesignPatterns.Iterator
+{
+    public interface IIterator 
+    {
+        int getNext();
+        bool hasMore();
+    }
+}
